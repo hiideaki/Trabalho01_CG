27e0b3e88813131f6cf42776bc74fd8f8a4b0565
Marcelo <hideaki09@hotmail.com>
2017-08-29 23:39:55 -0300

    Interface melhorada, filtros melhorados

--- ProcessamentoImagem.java
+++ ProcessamentoImagem.java
@@ -10,12 +10,15 @@ public class ProcessamentoImagem extends JFrame implements ActionListener {
 	JLabel lblImg1, lblImg2;
 	JComboBox filtersList;
 	JButton btnConfirmar = new JButton("Confirmar");
+	JPanel pnlImg = new JPanel();
+	JPanel pnlLista = new JPanel();
+	JPanel pnlConfig = new JPanel();
+	
 	
 	ProcessamentoImagem() {
 		super("Processamento de Imagens");
-		setLayout(new GridLayout(2, 2));
 		
-		String[] filters = {"Grayscale", "Negativo", "Mosaico", "Contorno"};
+		String[] filters = {"Grayscale", "Negativo", "Pixelate", "Wave", "Focus", "Contour"};
 		filtersList = new JComboBox<String>(filters);
 		filtersList.setSelectedItem("Grayscale");
 		try {
@@ -25,19 +28,22 @@ public class ProcessamentoImagem extends JFrame implements ActionListener {
 	        g2d.drawImage(imgInput, 0, 0, 500, 500, null);
 	        g2d.dispose();
 		    Img2 = grayscale(Img1);
-		    Img3 = contour(Img1);
-		    Img4 = mosaic(Img1);
 	    } catch(Exception e) {
 	    	e.printStackTrace();
 	    }
 		lblImg1 = new JLabel(new ImageIcon(Img1));
 		lblImg2 = new JLabel(new ImageIcon(Img2));
 		
-		add(lblImg1);
-		add(lblImg2);
-		add(filtersList);
+		pnlImg.add(lblImg1, BorderLayout.WEST);
+		pnlImg.add(lblImg2, BorderLayout.EAST);
+		pnlLista.setLayout(new GridLayout(2, 0, 10, 10));
+		pnlLista.add(filtersList);
+		pnlLista.add(btnConfirmar);
+		pnlLista.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		
-		add(btnConfirmar);
+		add(pnlImg, BorderLayout.NORTH);
+		add(pnlLista, BorderLayout.WEST);
+	
 		btnConfirmar.addActionListener(this);
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -55,31 +61,20 @@ public class ProcessamentoImagem extends JFrame implements ActionListener {
 				Img2 = negativo(Img1);
 				break;
 			case 2:
-				Img2 = mosaic(Img1);
+				Img2 = pixelate(Img1);
 				break;
 			case 3:
+				Img2 = wave(Img1);
+				break;
+			case 4:
+				Img2 = focus(Img1);
+				break;
+			case 5:
 				Img2 = contour(Img1);
 				break;
 		}
 		lblImg2.setIcon(new ImageIcon(Img2));
 	}
-	
-	public int sat(int pixel) {
-	    int red = (pixel >> 16) & 0xff;
-	    int green = (pixel >> 8) & 0xff;
-	    int blue = (pixel) & 0xff;
-	    int med = (red + green + blue) / 3;
-	    if(med < 126) {
-	    	red = 0;
-	    	green = 0;
-	    	blue = 0;
-	    } else {
-	    	red = 255;
-	    	green = 255;
-	    	blue = 255;
-	    }
-	    return red * 65536 + green * 256 + blue;
-    }
 	
 	BufferedImage grayscale(BufferedImage img) {
 		int col, row, rgb, red, green, blue, med;
@@ -94,34 +89,6 @@ public class ProcessamentoImagem extends JFrame implements ActionListener {
 			    blue = (rgb) & 0xff;
 			    med = (red + green + blue) / 3;
 				ret.setRGB(i, j, med * 65536 + med * 256 + med);			
-			}
-		}
-		return ret;
-	}
-	
-	BufferedImage contour(BufferedImage img) {
-		int col, row, rgb, red, green, blue, g;
-		row = img.getHeight();
-		col = img.getWidth();
-		BufferedImage ret = new BufferedImage(col, row, BufferedImage.TYPE_INT_RGB);
-		for(int i = 0; i < row; i++) {
-			for(int j = 0; j < col; j++) {
-				rgb = img.getRGB(i, j);
-			    
-			    rgb = sat(rgb);
-		    	ret.setRGB(i, j, rgb);
-			    if(rgb == 0 && j >= 3) {
-			    	ret.setRGB(i, j - 3, 0xffffff);
-			    }
-			}
-		}
-		for(int j = 2; j < col; j++) {
-			for(int i = 2; i < row; i++) {
-				rgb = ret.getRGB(i, j);
-				g = (rgb >> 16) & 0xff;
-				if(g == 0) {
-					ret.setRGB(i - 1, j, 0);
-				}
 			}
 		}
 		return ret;
@@ -145,9 +112,9 @@ public class ProcessamentoImagem extends JFrame implements ActionListener {
 		return ret;
 	}
 	
-	BufferedImage mosaic(BufferedImage img) {
-		int col, row, rgb, red, green, blue, pix, somar, somag, somab;
-		pix = 10;
+	BufferedImage pixelate(BufferedImage img) {
+		int col, row, rgb, red, green, blue, somar, somag, somab;
+		int pix = 5; //tamanho dos blocos
 		col = img.getWidth() / pix;
 		row = img.getHeight() / pix;
 		BufferedImage ret = new BufferedImage(col * pix, row * pix, BufferedImage.TYPE_INT_RGB);
@@ -177,6 +144,118 @@ public class ProcessamentoImagem extends JFrame implements ActionListener {
 		}
 		return ret;
 	}
+	
+	BufferedImage wave(BufferedImage img) {
+		int col, row, rgb, red, green, blue, med, ctrl;
+		row = img.getHeight();
+		col = img.getWidth();
+		BufferedImage ret = new BufferedImage(col, row, BufferedImage.TYPE_INT_RGB);
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < col; j++) {
+				rgb = img.getRGB(i, j);
+				blue = (rgb) & 0xff;
+				rgb -= blue;
+				blue *= 1.5;
+				if(blue > 255) blue = 255;
+				rgb += blue;
+				ret.setRGB(i, j, rgb);
+			}
+		}
+		ctrl = 1;
+		double offset = 0;
+		double aux;
+		int dg = 10; //quantos graus "anda" por "i"
+		int dt = 10; //distorção
+		for(int i = 0; i < row; i++) {
+			aux = Math.sin(dg * i * Math.PI / 180);
+			if(aux >= 0) {
+				for(int j = col - 1; j > (int)(aux * dt); j--) {
+					rgb = ret.getRGB(j - (int)(aux * dt), i);
+					ret.setRGB(j, i, rgb);
+				}
+			} else {
+				for(int j = 0; j < col + (int)(aux * dt); j++) {
+					rgb = ret.getRGB(j - (int)(aux * dt), i);
+					ret.setRGB(j, i, rgb);
+				}
+			}
+		}
+		return ret;
+	}
+	
+	int dist(int x1, int y1, int x2, int y2) {
+		return (int)Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	}
+	
+	BufferedImage focus(BufferedImage img) {
+		int col, row, rgb, red, green, blue, med;
+		double t, tmax;
+		row = img.getHeight();
+		col = img.getWidth();
+		tmax = dist(row / 2, col / 2, 0, 0);
+		BufferedImage ret = new BufferedImage(col, row, BufferedImage.TYPE_INT_RGB);
+		for(int i = 0; i < row - 1; i++) {
+			for(int j = 0; j < col - 1; j++) {
+				t = Math.abs((tmax - dist(i, j, row / 2, col / 2)) / tmax);
+				rgb = img.getRGB(i, j);
+			    red = (int)Math.floor(t * ((rgb >> 16) & 0xff));
+			    green = (int)Math.floor(t * ((rgb >> 8) & 0xff));
+			    blue = (int)Math.floor(t * ((rgb) & 0xff));
+			    
+			    if(red > 255) red = 255;
+			    if(green > 255) green = 255;
+			    if(blue > 255) blue = 255;
+			    
+			    med = (red + green + blue) / 3;
+				ret.setRGB(i, j, red * 65536 + green * 256 + blue);			
+			}
+		}
+		return ret;
+	}
+	
+	BufferedImage contour(BufferedImage img) {
+		int col, row, rgb, red, green, blue, med;
+		row = img.getHeight();
+		col = img.getWidth();
+		BufferedImage ret = new BufferedImage(col, row, BufferedImage.TYPE_INT_RGB);
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < col; j++) {
+				rgb = img.getRGB(i, j);
+
+				red = (rgb >> 16) & 0xff;
+			    green = (rgb >> 8) & 0xff;
+			    blue = (rgb) & 0xff;
+			    med = (red + green + blue) / 3;
+			    if(med < 126) {
+			    	red = 0;
+			    	green = 0;
+			    	blue = 0;
+			    } else {
+			    	red = 255;
+			    	green = 255;
+			    	blue = 255;
+			    }
+			    rgb = red * 65536 + green * 256 + blue;
+		    	ret.setRGB(i, j, rgb);
+			    if(rgb == 0 && j >= 3) {
+			    	ret.setRGB(i, j - 3, 0xffffff);
+			    }
+			}
+		}
+		
+		for(int j = 2; j < col; j++) {
+			for(int i = 2; i < row; i++) {
+				rgb = ret.getRGB(i, j);
+				green = (rgb >> 16) & 0xff;
+				if(green == 0) {
+					ret.setRGB(i - 1, j, 0);
+				}
+			}
+		}
+		return ret;
+	}
+	
+
 
 	public static void main(String[] args) {
 		new ProcessamentoImagem();
